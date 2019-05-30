e59ee43bb6dc558b022c606c230f4eee09f05811
alexandru.agape <alexandru.agape@student.tugraz.at>
2019-05-30 22:24:33 +0200

    PID algorithm. Measuring speed. Sending speed to android app from arduino

--- ArduinoProgram/grinding_machine/grinding_machine.ino
+++ ArduinoProgram/grinding_machine/grinding_machine.ino
@@ -1,9 +1,23 @@
 #include <SoftwareSerial.h>
+#include <PID_v1.h>
 
 // Bluetooth connection variables
+#define PIN_INPUT 4
+#define PIN_OUTPUT 9
+
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+double Kp=0.25, Ki=1.1, Kd=0;
+
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 unsigned int D2 = 2;
 unsigned int D3 = 3;
 unsigned int D4 = 4;
+unsigned int D5 = 5;
+unsigned int D6 = 6;
+unsigned int D9 = 9;
 SoftwareSerial BTSerial(D3,D2); // RX: port D2, TX port D3
 byte MESSAGE_START = 0x3C; // <
 byte MESSAGE_END = 0x3E; // >
@@ -30,19 +44,47 @@ char SPEED_CHANGE_COMMAND[] = "spd_cmd";
 char SPEED_VALUE[] = "spd_val";
 char BATTERY_VALUE[] = "bat_val";
 
+unsigned long oldTime = 0;
+unsigned long newTime = 0;
+unsigned long oldSendingTime = 0;
+unsigned long newSendingTime = 0;
+unsigned long interval = 500;
+
+float diskRPM = 0;
+int unlock = 1;
+
+
 //====================================================================================================================
 
 void setup() {
     Serial.begin(9600);
     Serial.println("<Arduino is ready>");
-    pinMode(D4, OUTPUT);  // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode if needed
-    digitalWrite(D4, HIGH);
+    pinMode(D4, INPUT);  // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode if needed
+    pinMode(D5, OUTPUT);
+    pinMode(D6, OUTPUT);
+    pinMode(D9, OUTPUT);
+    digitalWrite(D5, HIGH);
+    digitalWrite(D6, LOW);
     BTSerial.begin(9600);  // HC-05 default speed in AT command more
+
+    //initialize the variables we're linked to
+ Input = 0;
+ Setpoint = 0;
+
+ //turn the PID on
+ myPID.SetMode(AUTOMATIC);
 }
 
 //====================================================================================================================
 
 void loop() {
+
+
+    //analogWrite(D9,receivedSpeedValue);
+
+    PID_algorithm();
+
+    
     recvFromBTWithStartEndMarkers();
     showReceivedDataFromBT();
     
@@ -50,6 +92,15 @@ void loop() {
     sendReceivedDataFromSerialMonitor();
 
     //sendMessage(SPEED_VALUE, convertIntToChar(267));
+
+    measuringSpeed(2);
+    //sendSpeedToApplication();
+
+    Serial.println(diskRPM);
+    
+
+    //Serial.println(oldTime);
+    //Serial.println();
 }
 
 //====================================================================================================================
@@ -143,10 +194,7 @@ void sendReceivedDataFromSerialMonitor(){
             Serial.write(receivedBytesFromSerial[n]);
         }
         //BTSerial.write(receivedBytesFromSerial, numReceivedFromSerial);
-        char* message;
-        convertIntToChar(267, message);
-        sendMessage(SPEED_VALUE, message);
-        delete [] message;
+        //sendMessage(SPEED_VALUE, 267);
         Serial.println();
         newDataFromSerial = false;
     }
@@ -154,14 +202,17 @@ void sendReceivedDataFromSerialMonitor(){
 
 //====================================================================================================================
 
-void sendMessage(char message_type[], char message_val[]){
+void sendMessage(char message_type[], float message_val){
   // Add the message type and the value, separated by semi-colon
   char* constructedMessage;
-  if(constructMessage(message_type, message_val, constructedMessage) == true){
-    for (byte n = 0; n < numReceivedFromSerial; n++) {
+  Serial.println("Before if");
+  constructedMessage = constructMessage(message_type, message_val);
+  if(constructedMessage != 0){
+    for (byte n = 0; n < strlen(constructedMessage); n++) {
       BTSerial.write(constructedMessage[n]);
       Serial.write(constructedMessage[n]);
     }
+    Serial.println();
     delete[] constructedMessage;
   }else{
     return;    
@@ -171,21 +222,32 @@ void sendMessage(char message_type[], char message_val[]){
 
 //====================================================================================================================
 
-bool constructMessage(char message_type[], char message_val[], char* constructedMessage){
-  int constructedMessageLength = strlen(message_type) + strlen(message_val) + 4; // we need +4 because of \n, start, end, and separator
+char* constructMessage(char message_type[], float message_val){
+  String string_val = String(message_val);
+  char* constructedMessage = 0;
+  Serial.println("Value to send: " + string_val);
+  int constructedMessageLength;
+  String message_start = String((char)MESSAGE_START);
+  String message_separator = String((char)SEPARATOR);
+  String message_end = String((char)MESSAGE_END);
+  String constructedString = String("");
+
+  constructedString.concat(message_start);
+  constructedString.concat(message_type);
+  constructedString.concat(message_separator);
+  constructedString.concat(string_val);
+  constructedString.concat(message_end);
+  constructedMessageLength = constructedString.length()+1;
   if(constructedMessageLength >= numBytes){
-    return false;
+    return 0;
   }else{
     constructedMessage = new char[constructedMessageLength];
-    char message_start = (char)MESSAGE_START;
-    char message_separator = (char)SEPARATOR;
-    char message_end = (char)MESSAGE_END;
-    strcpy(constructedMessage, &message_start);
-    strcat(constructedMessage, message_type);
-    strcpy(constructedMessage, &message_separator);
-    strcat(constructedMessage, message_val);
-    strcpy(constructedMessage, &message_end);
-    return true;
+    constructedString.toCharArray(constructedMessage, constructedMessageLength);
+    Serial.print("After toCharArray, string is: ");
+    Serial.println(constructedMessage);
+    Serial.print("After toCharArray, with length: ");
+    Serial.println(strlen(constructedMessage));
+    return constructedMessage;
   }
 }
 
@@ -219,5 +281,41 @@ void convertIntToChar(int value, char* cstr){
 }
 
 //====================================================================================================================
+
+void measuringSpeed(int magnets){
+if(digitalRead(D4) == LOW && unlock == 1)
+    {
+     oldTime = newTime;
+     newTime = millis();
+    
+     diskRPM = (1.0/magnets)/((newTime-oldTime)/1000.0/60.0);
+
+     unlock  = 0;
+    }
+
+    if(digitalRead(D4) == HIGH && unlock == 0)
+    {
+      unlock = 1;
+    }  
+}
+
+//====================================================================================================================
+
+void PID_algorithm(){
+  Setpoint = receivedSpeedValue;
+  Input = diskRPM;
+  myPID.Compute();
+  analogWrite(D9,Output);
+}
+
+//====================================================================================================================
+
+void sendSpeedToApplication(){
+  newSendingTime = millis();
+  if(newSendingTime - oldSendingTime >= interval){
+    sendMessage(SPEED_VALUE, diskRPM);
+    oldSendingTime = newSendingTime;
+  }
+}
 
 // Functions to update the speed value of the motor and the battery level (if possible).
